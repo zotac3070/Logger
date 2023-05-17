060bd3e934206361da07b27b5d660b9c8e136e93
server_RTX3090 <your_email_address@example.com>
2023-05-17 20:21:44 +0400

    Final commit

--- logger.py
+++ logger.py
@@ -4,6 +4,7 @@ import smtplib
 import ssl
 from email.message import EmailMessage
 from threading import Thread
+import json
 
 
 class LogLevel(Enum):
@@ -19,14 +20,26 @@ class Logger:
         self.log_targets = []
         self.lock = threading.Lock()
         self.sender = Sender()
-        self.receivers_emails = ['mariam.harutyunyan.ca@gmail.com', 
-                                 'edik.mughdusyan.ca@gmail.com',  
-                                 'mariam.harutyunyan.ca@gmail.com',
-                                 'armtatevik2015@gmail.com']
+        self.receivers_emails = []
+        self.json = self.read_data()
+        self.recievers_json = self.json['log_levels']
+        
+        for email in self.recievers_json.values():
+            self.receivers_emails.append(email)
+        
+        self.sender_cred = self.json['sender']
         
         self.console_target = ConsoleLogTarget()
         self.add_log_target(self.console_target)
 
+    def read_data(self):
+        with open('data.json', 'r') as file:
+            # Step 3: Read the file content
+            content = file.read()
+            # Step 4: Parse the JSON
+            data = json.loads(content)
+        return data
+        
     def add_log_target(self, log_target):
         self.log_targets.append(log_target)
 
@@ -36,22 +49,20 @@ class Logger:
     def _log(self, message, log_level=LogLevel.INFO):
         if log_level.value < self.min_log_level.value:
             return
-        print('aaa')
         with self.lock:
             for i, log_target in enumerate(self.log_targets):
                 log_target.log(message, log_level)
-                self.sender.send_email(self.receivers_emails[log_level.value - 1], message)
+                self.sender.send_email(self.receivers_emails[log_level.value - 1], message, self.sender_cred)
     
     def log(self, message, log_level=LogLevel.INFO):
-        print('aaa')
-        threading.Thread(target=worker, args=[message, log_level], daemon=True).start()
+        threading.Thread(target=self._log, args=[message, log_level]).start()
 
 
 class Sender:
-    def send_email(self, email_receiver ,message):
-        email_sender = 'tatev.avetisyan.ca@gmail.com'
+    def send_email(self, email_receiver, message, sender):
+        email_sender = sender['email']
         #Enable 2-step verification and provide the generated password instead of the account password in the code.
-        email_password = 'sooyejpqmvevosgm'
+        email_password = sender['password']
         # Set the subject and body of the email
         subject = 'Logger'
         body = message
